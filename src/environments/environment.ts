8d8355de36b425a8af2dca8ec8bbc17a55206e26
Averi Yu / Zhizhen Yu <73741014+averi-u@users.noreply.github.com>
2025-10-18 17:39:41 -0700

    firebase keys

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,17 @@
-// src/environments/environment.ts
-export const environment = {
-  production: false,
-  firebase: {
-    apiKey: "…",
-    authDomain: "your-app.firebaseapp.com",
-    projectId: "your-app",
-    storageBucket: "your-app.appspot.com",
-    messagingSenderId: "…",
-    appId: "…"
-  }
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBERN4RS8NRrrejh_tGO40IQ-6Pp0NsPM4",
+  authDomain: "coffee-spark-ai-barista-e7997.firebaseapp.com",
+  projectId: "coffee-spark-ai-barista-e7997",
+  storageBucket: "coffee-spark-ai-barista-e7997.firebasestorage.app",
+  messagingSenderId: "393880096819",
+  appId: "1:393880096819:web:7a0c620f036a7d96881346"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
