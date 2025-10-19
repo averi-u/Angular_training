e519e6da9033af8bbfc438a1c07add2a92d85ab5
Averi Yu / Zhizhen Yu <73741014+averi-u@users.noreply.github.com>
2025-10-18 17:37:57 -0700

    firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+// src/environments/environment.ts
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "…",
+    authDomain: "your-app.firebaseapp.com",
+    projectId: "your-app",
+    storageBucket: "your-app.appspot.com",
+    messagingSenderId: "…",
+    appId: "…"
+  }
+};
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
+// src/main.ts
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
+
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { environment } from './environments/environment';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ],
-}).catch(err => console.error(err));
+}).catch(console.error);
